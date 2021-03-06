f56ffdf9901497f179eec8acc49243178db834c2
unknown <59224888+zarmintaa@users.noreply.github.com>
2021-03-05 20:50:52 -0800

    all

--- Intent/MyIntentApp/app/src/main/java/com/example/myintentapp/MainActivity.kt
+++ Intent/MyIntentApp/app/src/main/java/com/example/myintentapp/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.example.myintentapp
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnMoveActivity: Button = findViewById(R.id.btn_move_activity)
+        btnMoveActivity.setOnClickListener(this)
+
+        val btnMoveWithDataActivity: Button = findViewById(R.id.btn_move_activity_data)
+        btnMoveWithDataActivity.setOnClickListener(this)
+
+        val btnDialPhone: Button = findViewById(R.id.btn_dial_number)
+        btnDialPhone.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.btn_move_activity -> {
+                val moveIntent = Intent(this@MainActivity, MoveActivity::class.java)
+                startActivity(moveIntent)
+            }
+            R.id.btn_move_activity_data -> {
+                val moveWithDataObject = Intent(this@MainActivity, MoveWithDataActivity::class.java)
+                moveWithDataObject.putExtra(MoveWithDataActivity.EXTRA_NAME, "Dicoding")
+                moveWithDataObject.putExtra(MoveWithDataActivity.EXTRA_AGE, 5)
+                startActivity(moveWithDataObject)
+            }
+            R.id.btn_dial_number -> {
+                val phoneNumber = "081210841382"
+                val dialPhoneIntent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:$phoneNumber"))
+                startActivity(dialPhoneIntent)
+            }
+        }
     }
 }
